cc25feae75b51d5778d39927417987ac9c4f0a25
Your Name <you@example.com>
2023-03-09 18:11:37 -0500

    commit

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -9,7 +9,7 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active-btn");
     mobileMenu.classList.toggle("active-menu");
     if (menuBtn.classList.contains("active-btn")) {
-        icon.src = (".mobile-menu-btn img")
+        icon.src = ("./images/icon-close-menu.svg")
     } else {
         icon.src = "./images/icon-menu.svg";
     }
@@ -23,7 +23,7 @@ navLinks.forEach((link) => {
       subMenu.classList.toggle("active-menu");
     });
   });
-  
+
   mobileLinks.forEach((link) => {
     const subMenu = link.querySelector(".mobile-sub");
     link.addEventListener("click", () => {
