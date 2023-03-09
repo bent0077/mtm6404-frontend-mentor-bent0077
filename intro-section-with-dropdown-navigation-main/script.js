1c962b71eccd60e2c2968bd4d2df2e921e9ed942
Your Name <you@example.com>
2023-03-09 17:53:38 -0500

    commit

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -1,5 +1,15 @@
-const navLinks= document.querySelectorAll("nav-links li")
-const mobileLinks= document.querySelectorAll("mobile-links li")
-const menuBtn= document .querySelector(".mobile-menu-btn")
-const mobileMenu= document .querySelector(".mobile-menu")
-const overFlow= document .querySelector(".overflow")
+const navLinks= document.querySelectorAll("nav-links li");
+const mobileLinks= document.querySelectorAll("mobile-links li");
+const menuBtn= document .querySelector(".mobile-menu-btn");
+const mobileMenu= document .querySelector(".mobile-menu");
+const overFlow= document .querySelector(".overflow");
+
+menuBtn.addEventListener("click", () =>{
+const icon = document.querySelector(".mobile-menu-btn img");
+menuBtn.classList.toggle("active-btn");
+mobileMenu.classList.toggle("active-menu");
+if(menuBtn.classList. contains("active-btn"))
+
+
+}
+)
