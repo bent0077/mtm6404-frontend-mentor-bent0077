8b2ee718c3c855dfc18c77d4a37e56893f17f413
Your Name <you@example.com>
2023-03-09 17:49:07 -0500

    js

---
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,5 @@
+const navLinks= document.querySelectorAll("nav-links li")
+const mobileLinks= document.querySelectorAll("mobile-links li")
+const menuBtn= document .querySelector(".mobile-menu-btn")
+const mobileMenu= document .querySelector(".mobile-menu")
+const overFlow= document .querySelector(".overflow")
